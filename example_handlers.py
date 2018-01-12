a9e76e3762af63c53968ae4c4d2fefdaccb009cd
Adam Tankanow <adam.tankanow@gmail.com>
2018-01-12 14:39:13 -0500

    CORE-152: working example handler

--- example_handlers.py
+++ example_handlers.py
@@ -3,11 +3,11 @@
 # Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 
 
-import pyfaaster.aws.decorators as decs
+import pyfaaster.aws.handlers_decorators as decs
 
 
-@decs.lambda_handler()
-def example__hello_world(event, context, namespace, configuration, client_details):
+@decs.default()
+def hello_world(event, context, **kwargs):
     return {
         'statusCode': 200,
         'body': 'Hello, World!'
