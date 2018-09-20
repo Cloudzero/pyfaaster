2b605c1aaa0dae7b2e4b540963e91e6951954d24
Adam Tankanow <adam.tankanow@gmail.com>
2018-09-20 12:15:53 -0400

    ISSUE-26: Ensure Expression Attribute Names/Values are Alphanumeric

--- pyfaaster/aws/dynamodb.py
+++ pyfaaster/aws/dynamodb.py
@@ -2,15 +2,20 @@
 # Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
 # Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 
+import re
+
+
+pattern = re.compile('[\W_]+', re.UNICODE)
+
 
 def update_item_from_dict(table, key, dictionary):
     """Update the item identified by `key` in the DynamoDB `table` by adding
     all of the attributes in the `dictionary`.
     """
-    updates_string = ', '.join([f'#{k} = :{k}' for k in dictionary.keys()])
+    updates_string = ', '.join([f'#{pattern.sub("", k)} = :{pattern.sub("", k)}' for k in dictionary.keys()])
     update_expression = f'SET {updates_string}'
-    attribute_names = {f'#{k}': k for k in dictionary.keys()}
-    attribute_values = {f':{k}': v for k, v in dictionary.items()}
+    attribute_names = {f'#{pattern.sub("", k)}': k for k in dictionary.keys()}
+    attribute_values = {f':{pattern.sub("", k)}': v for k, v in dictionary.items()}
     item = table.update_item(
         Key=key,
         UpdateExpression=update_expression,
